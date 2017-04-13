ccf4bcb74055e11c36f161a5fd969a4e14552336
Evan Faler <Evan Faler>
2017-04-12 21:55:42 -0400

    First Commit

---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        //Generate unique identifier
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
+
